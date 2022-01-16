8fcedfe9a282c3ad1b711dc38ac6a0419c8744d3
Podexus23 <santon23@gmail.com>
2022-01-17 01:12:47 +0300

    main functions works correctly

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return a - b
+  return +a - +b
 }
 
 function multiply(a, b) {
@@ -11,25 +11,23 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a - b
+  return (a / b).toFixed(3)
 }
 
 function operate(operator, a, b) {
   return operator(a, b)
 }
 
-console.log(operate(add, 2, 4))
-
 //work with screen
 const screen = document.querySelector('.screen');
 const screenOperand = screen.querySelector('.screen-operand');
+const screenResult = screen.querySelector('.screen-result');
 const numButtons = document.querySelectorAll('.buttons-numbers .button');
 const optionButtons = document.querySelectorAll('.buttons-screen .button');
 const operationsButtons = document.querySelectorAll('.buttons-operations .button');
 
-let firstNumber = 0;
+let firstNumber = '0';
 let secondNumber;
-let operation;
 
 function operandOnScreen(number) {
   if (firstNumber == 0) {
@@ -48,10 +46,13 @@ numButtons.forEach((num => num.addEventListener('click', (e) => {
     return
   }
 })))
-//only clean first number
+//clean first second end result screen
 function cleanScreen() {
   screenOperand.textContent = 0;
-  firstNumber = 0;
+  firstNumber = '0';
+  secondNumber = undefined;
+  screenResult.textContent = '0 ='
+  previousOp = undefined;
 }
 
 function backspaceNumber() {
@@ -70,5 +71,72 @@ optionButtons.forEach((button) => {
     if (e.target.textContent.toLowerCase() == 'back') {
       backspaceNumber()
     }
+  })
+})
+//operations 
+
+function resetOperand() {
+  firstNumber = '0';
+  screenOperand.textContent = '0';
+}
+
+let previousOp;
+let counter = 0;
+
+function runOperate(op) {
+  counter++
+  console.log(op, previousOp, firstNumber, secondNumber, counter, ' start')
+  if (!secondNumber) {
+    secondNumber = firstNumber;
+    screenResult.textContent = `${firstNumber} ${op}`
+    previousOp = op;
+    resetOperand();
+    return
+  } else {
+    if (previousOp == "+") {
+      secondNumber = `${operate(add, secondNumber, firstNumber )}`;
+      screenResult.textContent = `${secondNumber} ${op}`;
+      resetOperand();
+      previousOp = op;
+      return
+    }
+    if (previousOp == "-") {
+      secondNumber = `${operate(subtract, secondNumber, firstNumber)}`;
+      screenResult.textContent = `${secondNumber} ${op}`;
+      resetOperand();
+      previousOp = op
+      return
+    }
+    if (previousOp == "*") {
+      console.log(op, firstNumber, secondNumber, counter, previousOp, 'milti')
+      secondNumber = `${operate(multiply, secondNumber, firstNumber)}`;
+      screenResult.textContent = `${secondNumber} ${op}`;
+      resetOperand();
+      previousOp = op
+      return
+    }
+    if (previousOp == "/") {
+      console.log(op, firstNumber, secondNumber, counter, previousOp, ' divide')
+      secondNumber = `${operate(divide, secondNumber, firstNumber)}`;
+      screenResult.textContent = `${secondNumber} ${op}`;
+      resetOperand();
+      previousOp = op
+      return
+    }
+    if (previousOp == "=") {
+      screenResult.textContent = `${secondNumber} ${op}`;
+      resetOperand();
+      previousOp = op
+      return
+    }
+  }
+
+}
+
+operationsButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const operator = e.target.textContent;
+    runOperate(operator);
+
   })
 })
