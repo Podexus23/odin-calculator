3d3e9768cff5ef443b7ba770d8192138c04ef544
Podexus23 <santon23@gmail.com>
2022-01-16 22:57:08 +0300

    add event for numbers and remade buttons categories

--- main.js
+++ main.js
@@ -19,3 +19,29 @@ function operate(operator, a, b) {
 }
 
 console.log(operate(add, 2, 4))
+
+//work with screen
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.buttons-numbers .button')
+
+let operand = 0;
+
+function operandOnScreen(number) {
+  if (operand == 0) {
+    operand = number;
+  } else {
+    operand = `${operand}${number}`
+  }
+  let screenOperand = screen.querySelector('.screen-operand');
+  screenOperand.textContent = operand;
+}
+
+numbers.forEach((num => num.addEventListener('click', (e) => {
+  //turning off unused buttons in number category
+  if (!isNaN(parseInt(e.target.textContent))) {
+    console.log(e.target.textContent);
+    operandOnScreen(e.target.textContent)
+  } else {
+    return
+  }
+})))
